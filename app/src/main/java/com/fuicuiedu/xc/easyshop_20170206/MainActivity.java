1b696944f6f5ffcc07d4471b436a435e22b0d976
wxcican <wxcican@qq.com>
2017-02-06 15:45:45 +0800

    测试，更新项目

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/MainActivity.java
@@ -2,6 +2,7 @@ package com.fuicuiedu.xc.easyshop_20170206;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "大家好！", Toast.LENGTH_SHORT).show();
     }
 }
