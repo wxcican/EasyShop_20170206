5c94198f65e6a56107050047beb97f69a7079204
wxcican <wxcican@qq.com>
2017-02-07 10:29:39 +0800

    资源导入（图片，布局，颜色等）

--- app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/MainActivity.java
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "大家好！", Toast.LENGTH_SHORT).show();
     }
 }
