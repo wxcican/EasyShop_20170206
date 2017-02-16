343b4ea8c927ae761470fed48b13779ab95a55cd
wxcican <wxcican@qq.com>
2017-02-16 10:02:45 +0800

    市场页面，视图接口实现

---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_20170206/main/shop/ShopView.java
@@ -0,0 +1,43 @@
+package com.fuicuiedu.xc.easyshop_20170206.main.shop;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/2/16 0016.
+ */
+
+public interface ShopView {
+
+    //数据刷新---处理中
+    void showRefresh();
+
+    //数据刷新 --刷新出错
+    void showRefreshError(String msg);
+
+    //数据刷新 --刷新结束
+    void showRefreshEnd();
+
+    //数据刷新 --隐藏下拉刷新视图
+    void hideRefresh();
+
+    //加载更多 -- 加载中
+    void showLoadMoreLoading();
+
+    //加载更多 -- 加载发生错误
+    void showLoadMoreError(String msg);
+
+    //加载更多 -- 没有更多数据
+    void showLoadMoreEnd();
+
+    //隐藏加载更多的视图
+    void hideLoadMore();
+
+    //添加更多数据
+//    void addMoreData(List<GoodsInfo> data);
+
+    //添加刷新更多的数据
+//    void addRefreshData(List<GoodsInfo> data);
+
+    // 消息提示
+    void showMessage(String msg);
+}
